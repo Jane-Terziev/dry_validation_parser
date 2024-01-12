88da858fd5a755caa94fb159871c1d69bb9212bc
Jane-Terziev <janeterziev@gmail.com>
2024-01-12 07:21:49 +0100

    Added configuration helper class

--- lib/dry_validation_parser.rb
+++ lib/dry_validation_parser.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "dry_validation_parser/version"
+require_relative "dry_validation_parser/config/configuration"
+require_relative "dry_validation_parser/config/validation_configuration"
 
 module DryValidationParser
   class Error < StandardError; end
---
+++ lib/dry_validation_parser/config/validation_configuration.rb
@@ -0,0 +1,13 @@
+module DryValidationParser
+  module Config
+    module ValidationConfiguration
+      extend Configuration
+
+      define_setting :enable_required_validation, true
+      define_setting :enable_nullable_validation, true
+      define_setting :enable_enums, true
+      define_setting :enable_descriptions, true
+      define_setting :nullable_type, :"x-nullable"
+    end
+  end
+end
